232330e7e4547df5e968d0ded35785af0ccb4d63
chaitanya <cvsschaitanya@gmail.com>
2020-12-30 21:00:05 +0530

    Single group everyyone chats with username

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 usernameBox = document.querySelector("#username_box");
 messagebox = document.querySelector("#message_box");
 sendButton = document.querySelector("#send");
-chats = document.querySelector("#window");
+chats = document.querySelector("#chat_window");
+
+var messageseRef = null;
 
 function getMessageHTML(username, message) {
     return "<div class=\"message\">  " +
@@ -24,8 +26,16 @@ function send() {
     if (message === "") {
         return;
     }
-    messageHTML = getMessageHTML(username, message);
-    chats.innerHTML += messageHTML;
+
+    if (messagesRef === null) {
+        return;
+    }
+
+    messagesRef.push({
+        username: username,
+        message: message
+    });
+
     messagebox.value = "";
 }
 
@@ -35,3 +45,20 @@ document.addEventListener('keydown', function(event) {
         send();
     }
 })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    messagesRef = firebase.database().ref('messages');
+    messagesRef.on('value', (snapshot) => {
+        console.dir(snapshot);
+        chats.innerHTML = "";
+        snapshot.forEach(function(childSnapshot) {
+            convo = childSnapshot.val();
+            console.log(convo);
+            HTML = getMessageHTML(convo['username'], convo['message']);
+            chats.innerHTML += HTML;
+        })
+        allP = document.querySelectorAll(".text")
+        allP[allP.length - 1].scrollIntoView();
+    })
+});
